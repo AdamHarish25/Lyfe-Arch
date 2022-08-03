a0de1b3732b13006fa584ef765079b581b50620b
adam <adamharits25@gmail.com>
2022-08-03 10:18:46 +0700

    LyfeArch Developement 2.5

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -48,93 +48,92 @@ class Carousel extends Component {
 
   render() {
     return (
-      <div className="mt-5">
-        <div
-          onMouseEnter={() => {
-            this.setState({ paused: true });
-          }}
-          onMouseLeave={() => {
-            this.setState({ paused: false });
-          }}
-          className="w-screen md:max-w-3xl lg:max-w-4xl h-100 grid place-items-center overflow-hidden relative ring ring-transparent transform duration-300 hover:ring-blue-500 rounded-none md:rounded-lg ease-in-out"
-        >
-          <Swipe onSwipeLeft={this.nextSlide} onSwipeRight={this.prevSlide}>
-            {CarouselData.map((slide, index) => {
-              return (
-                <div
-                  className={`transform duration-200 ease-in-out w-full h-auto ${
-                    index === this.state.currentSlide
-                      ? "block object-cover"
-                      : "hidden"
-                  }`}
-                >
-                  <div className="w-auto h-auto grid place-items-center my-10">
+      <div
+        onMouseEnter={() => {
+          this.setState({ paused: true });
+        }}
+        onMouseLeave={() => {
+          this.setState({ paused: false });
+        }}
+        className="w-screen md:max-w-3xl lg:max-w-4xl h-110 grid place-items-center overflow-hidden relative ring ring-transparent transform duration-300 hover:ring-blue-500 rounded-none md:rounded-lg ease-in-out"
+      >
+        <Swipe onSwipeLeft={this.nextSlide} onSwipeRight={this.prevSlide}>
+          {CarouselData.map((slide, index) => {
+            return (
+              <div
+                className={`transform duration-200 ease-in-out w-full h-auto px-0 md:px-10 ${
+                  index === this.state.currentSlide
+                    ? "block object-cover"
+                    : "hidden"
+                }`}
+              >
+                <div className="w-auto h-auto grid place-items-center my-10">
+                  <img
+                    src={slide.Logo}
+                    className={slide.LogoSize}
+                    key={index}
+                  />
+                </div>
+                <div className="w-auto h-auto px-10 my-10">
+                  <p
+                    key={index}
+                    className="text-center font-Poppins font-normal text-xs sm:text-sm md:text-lg"
+                  >
+                    {slide.TextContained}
+                  </p>
+                </div>
+                <div className="w-full h-auto flex justify-center items-center">
+                  <div className="w-auto h-auto grid grid-cols-2 place-items-center">
                     <img
-                      src={slide.Logo}
-                      className={slide.LogoSize}
+                      src={slide.ProfileImg}
                       key={index}
+                      className="h-14 w-14 object-cover object-center ring-2 p-1 rounded-full"
                     />
-                  </div>
-                  <div className="w-auto h-auto px-10 my-10">
-                    <p className="text-center font-Poppins font-normal text-xs sm:text-sm md:text-lg">
-                      Most calendars are designed for teams. Slate is designed
-                      for freelancers <br className="hidden md:block" /> who
-                      want a simple way to plan their schedule.
-                    </p>
-                  </div>
-                  <div className="w-full h-auto flex justify-center items-center">
-                    <div className="w-auto h-auto grid grid-cols-2 place-items-center">
-                      <img
-                        src={slide.ProfileImg}
-                        key={index}
-                        className="h-14 w-14 object-cover object-center ring-2 p-1 rounded-full"
-                      />
-                      <div className="h-auto w-auto">
-                        <ul className="list-none space-y-3">
-                          <li>
-                            <h1 className="font-Poppins font-medium text-xs">
-                              Organize across
-                            </h1>
-                            <h1 className="font-Poppins font-normal text-xs">
-                              Ui designer
-                            </h1>
-                          </li>
-                        </ul>
-                      </div>
+                    <div className="h-auto w-auto">
+                      <ul className="list-none space-y-3">
+                        <li>
+                          <h1 className="font-Poppins font-medium text-xs">
+                            Organize across
+                          </h1>
+                          <h1 className="font-Poppins font-normal text-xs">
+                            Ui designer
+                          </h1>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </Swipe>
-          <div className="absolute w-full flex justify-center bottom-5">
-            {CarouselData.map((_element, index) => {
-              return (
-                <div
-                  className={`transform duration-300 ease-in-out ${
-                    index === this.state.currentSlide
-                      ? "h-2 w-5 bg-blue-500 rounded-full mx-2 mb-2 cursor-pointer"
-                      : "h-2 w-2 bg-gray-600/40 rounded-full mx-2 mb-2 cursor-pointer"
-                  }`}
-                  key={index}
-                  onClick={() => {
-                    this.setCurrentSlide(index);
-                  }}
-                ></div>
-              );
-            })}
-          </div>
-
-          <AiOutlineLeft
-            onClick={this.prevSlide}
-            className="absolute left-0 md:left-2 lg:left-5 text-5xl inset-y-1/2 text-black/30 hover:text-black cursor-pointer transform duration-200"
-          />
-
-          <AiOutlineRight
-            onClick={this.nextSlide}
-            className="absolute right-0 md:right-2 lg:right-5 text-5xl inset-y-1/2 text-black/30 hover:text-black cursor-pointer transform duration-200"
-          />
+              </div>
+            );
+          })}
+        </Swipe>
+        <div className="absolute w-full flex justify-center bottom-5">
+          {CarouselData.map((_element, index) => {
+            return (
+              <div
+                className={`transform duration-300 ease-in-out ${
+                  index === this.state.currentSlide
+                    ? "h-2 w-5 bg-blue-500 rounded-full mx-2 mb-2 cursor-pointer"
+                    : "h-2 w-2 bg-gray-600/40 rounded-full mx-2 mb-2 cursor-pointer"
+                }`}
+                key={index}
+                onClick={() => {
+                  this.setCurrentSlide(index);
+                }}
+              ></div>
+            );
+          })}
         </div>
+
+        <AiOutlineLeft
+          onClick={this.prevSlide}
+          className="absolute left-0 md:left-2 lg:left-5 text-5xl inset-y-1/2 text-black/30 hover:text-black cursor-pointer transform duration-200"
+        />
+
+        <AiOutlineRight
+          onClick={this.nextSlide}
+          className="absolute right-0 md:right-2 lg:right-5 text-5xl inset-y-1/2 text-black/30 hover:text-black cursor-pointer transform duration-200"
+        />
       </div>
     );
   }
--- src/Components/CarouselData.js
+++ src/Components/CarouselData.js
@@ -11,25 +11,35 @@ export const CarouselData = [
     Logo: IBMLogo,
     LogoSize: "h-10 w-24",
     ProfileImg: ProfilePic,
+    TextContained:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus accumsan lacus, et porta mauris rhoncus tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit."',
   },
   {
     Logo: KempDo,
     LogoSize: "h-20 w-40",
     ProfileImg: ProfilePic,
+    TextContained:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet hendrerit libero. Maecenas at mauris lacinia, congue tellus vitae, sagittis lectus."',
   },
   {
     Logo: KemPer,
     LogoSize: "h-20 w-28",
     ProfileImg: ProfilePic,
+    TextContained:
+      '"Aenean molestie purus vestibulum malesuada posuere. Vivamus egestas in ligula id consequat. Phasellus vel nulla congue, varius diam vestibulum, aliquet risus."',
   },
   {
     Logo: DPRRI,
     LogoSize: "h-24 w-44",
     ProfileImg: ProfilePic,
+    TextContained:
+      '"Aenean sit amet felis sem. Etiam commodo turpis ac semper rhoncus. Nulla et lorem nec lorem scelerisque iaculis. Ut malesuada luctus nibh, vel viverra eros consectetur nec."',
   },
   {
     Logo: Taqychan,
     LogoSize: "h-24 w-32",
     ProfileImg: ProfilePic,
+    TextContained:
+      '"Maecenas eget risus suscipit, tempor lorem vel, vestibulum magna. Proin fringilla a dolor quis tincidunt. Aliquam blandit faucibus turpis. Donec consectetur imperdiet neque id facilisis."',
   },
 ];
